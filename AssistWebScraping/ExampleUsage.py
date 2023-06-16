0531b69f79784732942393519443ca3cf2913a19
rkafle1 <rkafle@ucsd.edu>
2023-06-15 23:14:26 -0700

    created an example file

---
+++ AssistWebScraping/ExampleUsage.py
@@ -0,0 +1,19 @@
+from PDFGrabber import PDFGrabber
+from AssistAPIInformationGetter import *
+from pdfextractor import PDFExtractor
+from ProcessFile import *
+from writeToCSV import *
+from ProcessFile import *
+from pdfminer.high_level import extract_text
+# from Requirements import *
+import Requirements
+
+
+# this chunk downloads all CC agreements from UCI for CS.
+grabber = PDFGrabber(getSchoolID("University of California, Irvine"), Requirements.CollegeCSMajor.get("University of California, Irvine"), 
+                 'CS', 0.2)
+id_to_key = grabber.get_pdfs()
+
+# articulations will be the dictionary with the articulation mappings which is written to a csv in csvs/agreements directory
+articulations = CreateDictfromtxtUCI('agreements/report_120_3_CS.pdf', Requirements.IrvineReq)
+generateCSVfromAgreement(articulations, "csvs/agreements/report_120_3_CS.csv")
---
+++ AssistWebScraping/Requirements.py
@@ -0,0 +1,92 @@
+UCIReq = {"cs": [["I&C SCI 31 - Introduction to Programming (4.00)", "I&C SCI 32 - Programming with Software Libraries (4.00)", "I&C SCI 33 - Intermediate Programming (4.00)"]],
+             "math":[["MATH 2A - Single-Variable Calculus (4.00)"], ["MATH 2B - Single-Variable Calculus (4.00)"]]}
+
+UCSDReq = {"cs": [["CSE 8A - Introduction to Programming and Computational Problem Solving I (4.00)", "CSE 8B - Introduction to Programming and Computational Problem Solving II (4.00)" ], 
+                  ["CSE 11 - Introduction to Programming and Computational Problem Solving - Accelerated Pace(4.00)"], ["CSE 12 - Basic Data Structures and Object-Oriented Design (4.00) "], 
+                  ["CSE 15L - SoftwareTools and Techniques Laboratory (2.00)"], ["CSE 20 - Discrete Mathematics (4.00) Same-As: MATH 15A"], ["CSE 21 - Mathematics for Algorithms and Systems (4.00)"],
+                  ["CSE 30 - Computer Organization and Systems Programming (4.00)"]], 
+                  "math":[["MATH 18 - Linear Algebra (4.00)"], ["MATH 20A - Calculus for Scienceand Engineering (4.00)"], 
+                  ["MATH 20B - Calculus for Scienceand Engineering (4.00)"], ["MATH 20C - Calculus and Analytic Geometry for Science and Engineering (4.00)"]]}
+
+UCDReq = {"cs": [["ECS 020 - Discrete Mathematics For Computer Science(4.00)"], ["ECS 036A - Programming & Problem Solving (4.00)"],
+                 ["ECS 036B - Software Development & Object-Oriented Programming in C++ (4.00)"], ["ECS 036C - Data Structures, Algorithms, & Programming (4.00)"],
+                 ["ECS 050 - Computer Organization & Machine-Dependent Programming (4.00)"]], 
+        "math": [["MAT 021A - Calculus (4.00)"], ["MAT 021B - Calculus (4.00)"], ["MAT 021C - Calculus (4.00) "]]}
+
+UCLAReq = {"cs": [], "math": [["MATH 31A - Differentialand Integral Calculus (4.00)"], ["MATH 31B - Integration and InfiniteSeries (4.00) "]
+        ["MATH 32A - Calculus ofSeveral Variables (4.00)"], ["MATH 32B - Calculus ofSeveral Variables (4.00)"], ["MATH 33A - Linear Algebra and Applications (4.00)"],
+        ["MATH 33B - DifferentialEquations (4.00) "]], "Science": [["PHYSICS 1A+ 1B+1C+ 4AL or 4BL"]], "Other": [["ENGCOMP 3 - English Composition, Rhetoric, and Language(5.00)"]]}
+
+UCSBReq = {"cs": [["CMPSC 16 - Problem Solving with Computers I (4.00)"], ["CMPSC 24 - Problem Solving with Computers II (4.00)"], 
+            ["CMPSC 40 - Foundations of Computer Science(5.00)"]], 
+        "math": [["MATH 3A - Calculus with Applications, First Course(4.00) "], ["MATH 3B - Calculus with Applications, Second Course(4.00)"], 
+                ["MATH 4A - Linear Algebra with Applications (4.00)"], ["MATH 4B - DifferentialEquations (4.00)"]],
+        "science": [["PHYS 1 - Basic Physics (4.00)"], ["PHYS 2 - Basic Physics (4.00)"], ["PHYS 3 - Basic Physics (3.00)", "PHYS 3L - Physics Laboratory (1.00)"]]}
+
+UCSCReq = {"cs": [["CSE 30 - Programming Abstractions: Python (7.00)"], ["CSE 13S - Computer Systems and C Programming (7.00)"],
+                  ["CSE 12 - Computer Systems and Assembly Languageand Lab (7.00) "], ["CSE 16 - APPLIED DISCRETE MATHEMATICS(5.00)"]],
+            "math": [["MATH 19A - Calculus for Science, Engineering, and Mathematics (5.00)"], ["MATH 19B - Calculus for Science, Engineering, and Mathematics (5.00)"]]}
+
+UCRReq = {"cs": [["MATH 19B - Calculus for Science, Engineering, and Mathematics (5.00)"], ["CS 10B - Intro to Computer Sciencefor Science, Mathematics, and Engineering II (4.00)"]],
+          "math": [["MATH 9A - First-Year Calculus (4.00)", "MATH 9B - First-Year Calculus (4.00)", "MATH 9C - First-Year Calculus (4.00)"]],
+          "science": [["PHYS 40A - General Physics (5.00)"]]}
+
+UCMReq = {"cs": [["CSE 020 - Introduction to Computing I (2.00)", "CSE 021 - Introduction to Computing II (2.00)"], ["CSE 020 - Introduction to Computing I (2.00)", "CSE 021 - Introduction to Computing II (2.00)"]],
+          "math": [["MATH 021 - Calculus I for PhysicalSciences & Engineering (4.00) "], ["MATH 022 - Calculus II for PhysicalSciences & Engineering (4.00)"], ["MATH 023 - Vector Calculus (4.00)"], ["MATH 024 - Linear Algebra and DifferentialEquations (4.00)"]],
+          "science": [["PHYS 008 - Introductory Physics I for PhysicalSciences (3.00)", "PHYS 008L - Introductory Physics I for PhysicalSciences Lab (1.00)"], 
+                    ["PHYS 009 - Introductory Physics II for PhysicalSciences (3.00)", "PHYS 009L - Introductory Physics II for PhysicalSciences Lab (1.00)"]]}
+
+UCBReq = {"cs": [["COMPSCI 61A - TheStructureand Interpretation of Computer Programs (4.00)"], ["COMPSCI 61B - Data Structures (4.00)"], ["COMPSCI 61C - MachineStructures (4.00)"], ["COMPSCI 70 - Discrete Mathematics and Probability Theory (4.00)"],
+                ["EECS 16A - Designing Information Devices and Systems I (4.00)"]],
+            "math": [["MATH 1A - Calculus (4.00)"], ["MATH 1B - Calculus (4.00)"], ["MATH 53 - Multivariable Calculus (4.00)"], ["MATH 54 - Linear Algebra and DifferentialEquations (4.00)"]]}
+
+SDSUReq = {"cs": [["CS 150 - Introduction to Computer Programming (3.00)", "CS 150L - Introductory Computer Programming Laboratory (1.00)"], 
+                ["CS 160 - Intermediate Computer Programming (3.00)", "CS 160L - Intermediate Computer Programming Laboratory (1.00)"], ["CS 200 - Introduction to Data Scienceand Python (4.00)"],
+                ["CS 210 - Data Structures (3.00)"], ["CS 240 - Computer Organization (3.00)"]]}
+
+CSUSMReq = {"cs": [["CS 240 - Computer Organization (3.00)"], ["CS 211 - Computer ScienceII (4.00)"], ["CS 231 - Assembly Languageand Digital Circuits (4.00)"]],
+            "math": [["MATH 160 - Calculus with Applications, I (5.00)"], ["MATH 162 - Calculus with Applications, II (4.00)"], ["MATH 270 - Basic Discrete Mathematics (3.00)"],
+                    ["MATH 242 - Introduction to Statistics (3.00)"], ["MATH 264 - Introduction to Linear Algebra (3.00)"]]}
+
+CSULBReq = {"cs": [["CECS 105 - Introduction to Computer Engineering and Computer Science(1.00)"], ["CECS 174 - Introduction to Programming and Problem Solving (3.00)"],
+                   ["CECS 225 - Digital Logicand Assembly Programming (3.00)"], ["CECS 228 - DiscreteStructures with Computing Applications (3.00)"], ["CECS 229 - DiscreteStructures with Computing Applications II (3.00)"],
+                   ["CECS 274 - Data Structures (3.00) "], ["CECS 277 - Object Oriented Application Development (3.00)"], ["ENGR 101 - Introduction to Engineering Profession (1.00)"],
+                   ["ENGR 102 - AcademicSuccess Skills (1.00)"]],
+            "math": [["MATH 122 - Calculus I (4.00)"], ["MATH 123 - Calculus II (4.00)"]]}
+
+CSUFReq = {"cs": [["CPSC 120 - Introduction to Programming (3.00)"], ["CPSC 121 - Object-Oriented Programming (3.00)"], ["CPSC 131 - Data Structures (3.00)"]],
+           "math":[["MATH 150A - Calculus I (4.00)"], ["MATH 150B - Calculus II (4.00)"], ["MATH 170A - MathematicalStructures I (3.00)"],
+                   ["MATH 170B - MathematicalStructureII (3.00)"], ["MATH 338 - Stat Appl to NaturalSci (4.00)"]]}
+
+CSUSBReq = {"cs": [["CSE 2010 - Computer ScienceI (4.00)"], ["CSE 2020 - Computer ScienceII (4.00)"], ["CSE 2130 - Machine Organization (3.00)"]],
+            "math":[ ["MATH 2720 - Discrete Mathematics (3.00)"], ["MATH 2210 - Calculus I (4.00)", "MATH 2220 - Calculus II (4.00) "], 
+                    ["MATH 2265 - Statistics with Applications (3.00)"], ["MATH 2310 - Applied Linear Algebra (4.00) "]],
+            "science": [["PHYS 2500 - General Physics I (4.00)", "PHYS 2500L - General Physics I Lab (1.00)", "PHYS 2510 - General Physics II (4.00)", "PHYS 2510L - General Physics II Lab (1.00)"]]}
+
+CPPReq = {"cs": [["PHYS 2510L - General Physics II Lab (1.00)"], ["CS 2610 - Introduction to Cyber Security and Network"], ["STA 2260 - Probability and Statistics for Computer Scientists and Engineers (3.00)"],
+                ["CS 1300 - DiscreteStructures (3.00)", "CS 1400 - Introduction to Programming and Problem Solving (4.00)", "CS 2400 - Data Structures and Advanced Programming (4.00)", "CS 2640 - Computer Organization and Assembly Programming (3.00)"]],
+        "math": [["MAT 1140 - Calculus I (4.00)"], ["MAT 1150 - Calculus II (4.00) "]],
+        "science": [["BIO 1110 - LifeScience(2.00)", "BIO 1110L - LifeScienceLaboratory (1.00)"], ["BIO 1110 - LifeScience(2.00)", "BIO 1110L - LifeScienceLaboratory (1.00)"],
+                    ["PHY 1510 - Introduction to Newtonian Mechanics (3.00)", "PHY 1510L - Newtonian Mechanics Laboratory (1.00)"]]}
+
+CSUDHReq = {"cs":[["CSC 121 - Introduction to Computer Scienceand Programming I (4.00)"], ["CSC 123 - Introduction to Computer Scienceand Programming II (4.00)"],
+                  ["CSC 221 - Assembly Languageand Introduction to Computer Organization (3.00)"]],
+            "math": [["MAT 191 - Calculus I (5.00)"], ["MAT 193 - Calculus II (5.00)"], ["MAT 271 - Foundations of Higher Mathematics (3.00)"],
+                     ["MAT 281 - Discrete Mathematics (3.00)"]],
+            "science": [["PHY 130 - General Physics I (5.00)", "PHY 132 - General Physics II (5.00)"], ["PHY 130 - General Physics I (5.00)", "PHY 132 - General Physics II (5.00)"],
+                        ["PHY 130 - General Physics I (5.00)", "PHY 132 - General Physics II (5.00)", "PHY 134 - General Physics III (4.00)"]]}
+
+CSULAReq = {"cs": [["CS 1222 - Introduction to Relational Databases (3.00)"], ["CS 2011 - Introduction to Programming I (3.00)"], ["CS 2012 - Introduction to Programming II (3.00)"],
+                   ["CS 2013 - Programming with Data Structures (3.00)"], ["CS 2148 - DiscreteStructures (3.00) "], ["CS 2445 - Introduction to Computer Systems (3.00)"],
+                   ["CS 2470 - Fundamentals of Network Systems and Cybersecurity (3.00)"]],
+            "math": [["MATH 2110 - Calculus I (4.00)"], ["MATH 2120 - Calculus II (4.00)"], ["MATH 2550 - Introduction to Linear Algebra (3.00)"]],
+            "science":[["PHYS 2100 - General Physics I: Mechanics (5.00)"]], "other": [["ENGL 2030 - Introduction to Technical Writing (3.00)"]]}
+ 
+
+
+CollegeCSMajor = {"University of California, San Diego": "Computer Science B.S.", "University of California, Irvine": "Computer Science, B.S.",
+                   "University of California, Santa Barbara": "Computer Science, B.S.", "University of California, Riverside": "Computer Science, B.S.",
+                   "University of California, Berkeley": "Computer Science, Lower Division B.A.", "University of California, Davis": "Computer Science B.S.",
+                   "University of California, Los Angeles": "Computer Science/B.S.", "University of California, Santa Cruz": "Computer Science B.S.",
+                   "University of California, Merced": "Computer Science and Engineering, B.S.", "San Diego State University": "Computer Science",
+                   }
