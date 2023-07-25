0e4275062fcf3c6fbc85bfea5d922b5f3287d22f
rkafle1 <rkafle@ucsd.edu>
2023-07-25 09:02:47 -0700

    updated

--- AssistWebScraping/analysis/CSVHandling.py
+++ AssistWebScraping/analysis/CSVHandling.py
@@ -1,6 +1,24 @@
 import csv
 from Requirements import *
 import os
+from Requirements import *
+import ast
+import pandas as pd
+
+def delete_empty_rows(csv_file, output_file):
+    # Read the CSV file into a pandas DataFrame
+    df = pd.read_csv(csv_file)
+
+    # Drop rows with all NaN (empty) values
+    df.dropna(how='all', inplace=True)
+
+    # Write the modified DataFrame back to a new CSV file
+    df.to_csv(output_file, index=False)
+delete_empty_rows("csvs/UniSheets/UCI.csv", "deletedUCI.csv")
+
+
+
+
 UniNameShort = ["CSUF", "Sonoma","CPSLO", "Chico", "CSUSM", "SDSU", "SJSU", "CSULA", "CSUMB", "CSUN", "CSUB", "CSUSB", "CSUDH", "CSUEB", "CSUStan", "CSUS"
                 , "SFSU", "Humboldt", "CSUFresno", "CSULB", "CSUCI", "CPP", "UCI", "UCB", "UCSD", "UCM", "UCSC", "UCSB", "UCD", "UCLA", "UCR"]
 
@@ -9,9 +27,11 @@ UniNameShort = ["CSUF", "Sonoma","CPSLO", "Chico", "CSUSM", "SDSU", "SJSU", "CSU
 def FixCSV(uniName, CSVFileName):
     with open("csvs/UniSheets/" + uniName + ".csv", 'w') as fixedCSV:
         writer = csv.writer(fixedCSV)
-
+        
         with open(CSVFileName + ".csv", 'r') as csvfile:
+            
             reader = csv.reader(csvfile)
+           
             prevCC = ''
             coursecnt = 0
             for row in reader:
@@ -87,48 +107,64 @@ def FixCSV(uniName, CSVFileName):
 def Fixall(UniList):
     for i in UniList:
         FixCSV(i, "csvs/csvs/Assist PDF scraped agreements - " + i)
+        
 # [['CSE 12 - Basic Data Structures and Object-Oriented Design (4.00)', 'somthing']]
 Fixall(UniNameShort)
 def getListfromString(str):
- 
-    if '[' == str[0] or '"' == str[0]:
-        lst = getListfromString(str[1:len(str) - 1])
-    else:
-        lst = []
-        elementstart = -1
-        for i in range(len(str)):
-            if str[i] == "'" and elementstart == -1:
-                elementstart = i
+    return ast.literal_eval(str)
+print(getListfromString("[['MATH 2A - Single-Variable Calculus (4.00)', 'MATH 2B - Single-Variable Calculus (4.00)']]"))
 
-            elif str[i] == "'":
-                lst.append(str[elementstart+1:i])
-                elementstart
-    
+def getrelList(UniName):
+    Reqs = []
+    with open("csvs/ReqRelationships.csv", 'r') as f:
+        reader = csv.reader(f, delimiter='/')
+        for row in reader:
+            if row[0] == UniName:
+                print(row[1])
+                Reqs.append(getListfromString(row[1]))
+    return Reqs
 
-
-# def getrelList(UniName):
-#     Reqs = []
-#     with open("csvs/ReqRelationships.csv", 'r') as f:
-#         reader = csv.reader(f)
-#         for row in reader:
-#             if row[0] == UniName:
-                
+def getallRelatedCourses(reqlist):
+    courses = []
+    for i in reqlist:
+        for j in i:
+            for z in j:
+                courses.append(z)
+    return courses
 # Fixes the format of the csv to capture full requirements
 # relList format: [[cse 8A, CSE 8B], [CSE 11]]
-# def ConvertToGradReqs(CSVFileName, relList, UniName):
-#     # create a new CSV file for the final sheet
-#     AddedFromrelList = []
-#     with open(CSVFileName + "_Final.csv", 'w') as csvfinal:
-#         # open the scraped csv file
-#         with open(CSVFileName + ".csv", 'r') as csv:
-#             reader = csv.reader(csv)
-#             # iterate through the scraped csv file
-#             for row in reader:
-#                 # check if the req courses are in the rel list
-#                 for i in relList:
-#                     for j in i:
-#                         if row[1] in j:
-
+def ConvertToGradReqs(CSVFileName, relList, UniName):
+    # create a new CSV file for the final sheet
+    AddedFromrelList = []
+    artslist= []
+    
+    prevCC = ""
+    with open(UniName + "Gradreqs.csv", 'w') as csvfinal:
+        writer = csv.writer(csvfinal)
+        # open the scraped csv file
+        with open(CSVFileName + ".csv", 'r') as csvr:
+            reader = csv.reader(csvr)
+            # iterate through the scraped csv file
+            for row in reader:
+                if row[0] != prevCC:
+                    for i in range(len(AddedFromrelList)):
+                        writer.writerow([row[0], AddedFromrelList[i], artslist[i]])
+                    AddedFromrelList = []
+                    artslist= []
+                    
+                # check if the req courses are in the rel list
+                # rel list will be : [[['CSE 12 - Basic Data Structures and Object-Oriented Design (4.00)']], [['CSE 15L - Software Tools and Techniques Laboratory (2.00)']]]
+                for i in relList:
+                    for j in i:
+                        if row[1] in j:
+                            if row[1] not in getallRelatedCourses(AddedFromrelList):
+                                AddedFromrelList.append(i)
+                                artslist.append([row[2]])
+                        else:
+                            artslist[len(artslist) - 1].append(row[2])
+                prevCC = row[0]
+ConvertToGradReqs("csvs/UniSheets/UCI", getrelList("UCI"), "UCI")
+                                
             # if so handle the relationship and write to new csv
             # else write that row to the new csv
             # add units in list form to 4th col.
