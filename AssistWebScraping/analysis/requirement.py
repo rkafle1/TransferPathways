3e6540fe1c9d51be60cbf66b5177e6d11d0643d2
Jinya Jiang <j9jiang@ucsd.edu>
2023-07-28 17:34:59 -0700

    add violin graphs and changed some function names

--- AssistWebScraping/analysis/requirement.py
+++ AssistWebScraping/analysis/requirement.py
@@ -21,6 +21,8 @@ import csv
 import CSVHandling
 import matplotlib.pyplot as plt
 import statistics
+import seaborn as sns
+import plotly.express as px
 
 import re
 import ast
@@ -114,15 +116,13 @@ def substitute_with_bool(expression, bool_list):
 
 
 
-def GetCountOfMetReqsDict(UniName):
-    
+def GetCountOfMetReqsUni(UniName):
     dict = {}
     # print(UniName)
     df = pd.read_csv("csvs/UniSheets/"+UniName+"Gradreqs.csv", header=None, sep='\t')
     row, column = df.shape
     print("row", row)
     print("column", column)
-
     total = 0
     for i in range(row):
         if (df.iloc[i,0] == df.iloc[0,0]):
@@ -162,36 +162,38 @@ def GetCountOfMetReqsDict(UniName):
 
 
 # produces dicts of met requirements for all unis and puts it into a csv
+#university and dictionary of met requirements
 def AllListsofMetReqsCount(UniList):
     fulldic = {}
-    with open("csvs/Findings/MetReqsCount.csv", 'w') as f:
+    with open("csvs/Findings/TotalMetReqsCount.csv", 'w') as f:
         writer = csv.writer(f, delimiter='\t')
         for uni in UniList: 
             # print(uni) 
             fulldic[uni] = []
-            dictreqs, _ = GetCountOfMetReqsDict(uni)
+            dictreqs, _ = GetCountOfMetReqsUni(uni)
             # print(dictreqs)
             fulldic[uni] = dictreqs
             writer.writerow([uni, dictreqs])
             dictreqs = []
             print("Done with ", uni , " count")
-
         return None
 
-def AllListsofMetReqsStats(UniList):
+#descriptions of count of met requirements
+def AllListsofMetReqsCountStats(UniList):
     fulldic = {}
-    with open("csvs/Findings/MetReqsStats.csv", 'w') as f:
+    with open("csvs/Findings/MetReqsCountStats.csv", 'w') as f:
         writer = csv.writer(f, delimiter='\t')
-        writer.writerow(["University", "Total", "max", "min", "median", "mean", "quantiles", "Requirement Values"])
+        writer.writerow(["University", "Total", "max", "min", "median", "mean", "quantiles", "Values"])
         for uni in UniList: 
             # print(uni) 
             fulldic[uni] = []
-            dictreqs, totalreq = GetCountOfMetReqsDict(uni)
+            dictreqs, totalreq = GetCountOfMetReqsUni(uni)
             # print(dictreqs)
             print("total reqs", totalreq)
             # fulldic["total"] = totalreq
 
             #analyze the data, get some basic data
+            #number of requirements met
             values = list(dictreqs.values())
             min_value = min(values)
             max_value = max(values)
@@ -204,53 +206,173 @@ def AllListsofMetReqsStats(UniList):
             fulldic[uni] = dictreqs
             # writer.writerow([uni, totalreq, max_value, min_value, median_value, mean_value, values])
 
-            writer.writerow([uni, totalreq, min_value, max_value, median_value, mean_value, quantiles, values])
+            writer.writerow([uni, totalreq, max_value, min_value, median_value, mean_value, quantiles, values])
             dictreqs = []
-            print("Done with ", uni , " stats")
+            print("Done with ", uni , " count")
+        return fulldic
+    
+#descriptions of percentage of met requirements
+def AllListsofMetReqsPercentageStats(UniList):
+    fulldic = {}
+    with open("csvs/Findings/MetReqsPercentageStats.csv", 'w') as f:
+        writer = csv.writer(f, delimiter='\t')
+        writer.writerow(["University", "Total", "max", "min", "median", "mean", "quantiles", "Values"])
+        for uni in UniList: 
+            # print(uni) 
+            fulldic[uni] = []
+            dictreqs, totalreq = GetCountOfMetReqsUni(uni)
+            # print(dictreqs)
+            print("total reqs", totalreq)
+            # fulldic["total"] = totalreq
+
+            #analyze the data, get some basic data
+            #number of requirements met
+            values = list(dictreqs.values())
+            values = [value / totalreq for value in values]
+            # print("values", values)
+            min_value = min(values)
+            max_value = max(values)
+            median_value = statistics.median(values)
+            mean_value = statistics.mean(values)
+            # if(len(values) > 4):
+            quantiles = np.percentile(values, [25, 50, 75])
+            # else:
+            #     quantiles = [0,0,0]
+            fulldic[uni] = dictreqs
+            # writer.writerow([uni, totalreq, max_value, min_value, median_value, mean_value, values])
+
+            writer.writerow([uni, totalreq, max_value, min_value, median_value, mean_value, quantiles, values])
+            dictreqs = []
+            print("Done with ", uni , " percentage")
         return fulldic
 
+#[,,,,]
 def getListOfValues(UniList):
     data = []
     for uni in UniList: 
             # print(uni) 
-        dictreqs, totalreq = GetCountOfMetReqsDict(uni)
+        dictreqs, totalreq = GetCountOfMetReqsUni(uni)
         values = list(dictreqs.values())
         data.append(values)
     print(data)
     return data
 
-def box_plotting():
+def transform_csv():
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv("csvs/Findings/MetReqsCountStats.csv", delimiter='\t')
 
-    # df = pd.read_csv(stats_path, header=None, sep='\t')
+    # Initialize an empty list to store the transformed data
+    transformed_data = []
 
-    data = getListOfValues(CSVHandling.UniNameShort)
-    fig = plt.figure(figsize =(10, 7))
-    # Creating axes instance
-    ax = fig.add_axes([0, 0, 1, 1])
-    ax = fig.add_subplot(111)
+    # Process the data and create the new format
+    for row in df.itertuples(index=False):
+        key = row[0]
+        values = row[7]
+        number_list = ast.literal_eval(values)
+        for value in number_list:
+            # print(value)
+            # if value != "," and value != "[" and value != "]" and value != " ":
+            transformed_data.append([key, value])
 
-    bp = ax.boxplot(data)
-    #add the scatter points
-    for i, d in enumerate(data):
-        x = np.random.normal(i + 1, 0.01, size=len(d))  # Spread out the points a bit for better visibility
-        ax.scatter(x, d, alpha=0.3)  # alpha controls the transparency of the scatter points
+    # Create a new DataFrame with the transformed data
+    new_df = pd.DataFrame(transformed_data, columns=['University', 'Values'])
 
-    #boxes - interquartile range (IQR)(the middle 50% of the data.) The horizontal line in the box - median, whiskers - 1.5 times the IQR, other points beyond - outliers 
-    plt.title("Boxplot of Met Requirements")
-    plt.xlabel("Universities")
-    plt.ylabel("Number of Requirements Met")
-    plt.xticks(np.arange(1, len(CSVHandling.UniNameShort)+1), CSVHandling.UniNameShort, rotation=45)
-    plt.show()
-# Show the plot
+    # Write the new DataFrame to a new CSV file
+    new_df.to_csv("csvs/Findings/violin.csv", index=False)
+
+# def box_plotting():
+
+#     # df = pd.read_csv(stats_path, header=None, sep='\t')
+
+#     data = getListOfValues(CSVHandling.UniNameShort)
+#     fig = plt.figure(figsize =(10, 7))
+#     # Creating axes instance
+#     ax = fig.add_axes([0, 0, 1, 1])
+#     ax = fig.add_subplot(111)
+
+#     bp = ax.boxplot(data)
+#     #add the scatter points
+#     for i, d in enumerate(data):
+#         x = np.random.normal(i + 1, 0.01, size=len(d))  # Spread out the points a bit for better visibility
+#         ax.scatter(x, d, alpha=0.3)  # alpha controls the transparency of the scatter points
+
+#     #boxes - interquartile range (IQR)(the middle 50% of the data.) The horizontal line in the box - median, whiskers - 1.5 times the IQR, other points beyond - outliers 
+#     plt.title("Boxplot of Met Requirements")
+#     plt.xlabel("Universities")
+#     # plt.ylabel("Number of Requirements Met")
+#     plt.ylabel("Percentage of Requirements Met")
+#     plt.xticks(np.arange(1, len(CSVHandling.UniNameShort)+1), CSVHandling.UniNameShort, rotation=45)
+#     plt.show()
+# # Show the plot
 
 
 
     
-
+#create the csv files for the met requirements
 # AllListsofMetReqsCount(CSVHandling.UniNameShort)
-# AllListsofMetReqsStats(CSVHandling.UniNameShort)
-box_plotting()
+# AllListsofMetReqsCountStats(CSVHandling.UniNameShort)
+# AllListsofMetReqsPercentageStats(CSVHandling.UniNameShort)
+# box_plotting()
+
+Countdf = pd.read_csv("csvs/Findings/MetReqsCountStats.csv", delimiter='\t')
+print(Countdf.head())
+
+#visualization of met requirements
+# violin graph for count
 
 
 
 
+figure, axis = plt.subplots(2, 2, figsize=(10, 8))
+sns.violinplot(data=Countdf, x = "Total", ax=axis[0,0])
+axis[0,0].set_title("Total requirements")
+sns.violinplot(data=Countdf, x = "max", ax=axis[0,1])
+axis[0,1].set_title("Max requirements")
+sns.violinplot(data=Countdf, x = "min", ax=axis[1,0])
+axis[1,0].set_title("Min requirements")
+sns.violinplot(data=Countdf, x = "median", ax=axis[1,1])
+axis[1,1].set_title("Median requirements")
+
+
+# Show the plot
+data = getListOfValues(CSVHandling.UniNameShort)
+figure2 = plt.figure(figsize =(10, 7))
+axis2 = figure2.add_axes([0, 0, 1, 1])
+axis2 = figure2.add_subplot(111)
+
+bp = axis2.boxplot(data)
+#add the scatter points
+for i, d in enumerate(data):
+    x = np.random.normal(i + 1, 0.01, size=len(d))  # Spread out the points a bit for better visibility
+    #labels , label=f'University {i+1}' not needed
+    axis2.scatter(x, d, alpha=0.3)  # alpha controls the transparency of the scatter points
+
+    #boxes - interquartile range (IQR)(the middle 50% of the data.) The horizontal line in the box - median, whiskers - 1.5 times the IQR, other points beyond - outliers 
+    axis2.set_title("Boxplot of Met Requirements")
+    axis2.set_xlabel("Universities")
+    # plt.ylabel("Number of Requirements Met")
+    axis2.set_ylabel("Percentage of Requirements Met")
+    axis2.legend()
+    axis2.set_xticklabels(CSVHandling.UniNameShort, rotation=45)
+ 
+
+transform_csv()
+df3 = pd.read_csv("csvs/Findings/violin.csv")
+print(df3.head())
+print(df3.columns)
+figure3 = plt.figure(figsize=(10, 8))
+axis3 = sns.violinplot(data = df3, x = "University", y = "Values")
+axis3.set_title("Total requirements met Violin plot")
+axis3.set_xlabel("Universities")
+axis3.set_ylabel("Percentage of Requirements Met")
+axis3.set_xticklabels(CSVHandling.UniNameShort, rotation=45)
+
+#px package
+figure4 = px.violin(df3, x="University", y="Values",  points="all", box=True, color="University")
+figure4.show()
+
+figure5 = px.strip(df3, x="University", y="Values")
+figure5.show()
+# plt.xticks(rotation=90)
+# Show both plots
+plt.show()
