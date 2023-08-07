4fcce0da5656e153398c7c6c76e473770d073887
rkafle1 <rkafle@ucsd.edu>
2023-08-07 11:51:22 -0700

    added missing CCs

--- AssistWebScraping/analysis/AnalyzingMapping.py
+++ AssistWebScraping/analysis/AnalyzingMapping.py
@@ -1,8 +1,9 @@
 import AnalyzeGradArtMapping
 # AnalyzeGradArtMapping.LeftoverGradReqs("LOWER") 
-AnalyzeGradArtMapping.LeftoverGradReqs("LowerCS") 
-AnalyzeGradArtMapping.LeftoverGradReqs("LowerMATH")
-AnalyzeGradArtMapping.LeftoverGradReqs("LowerMathCS")  
+# AnalyzeGradArtMapping.LeftoverGradReqs("LowerCS") 
+# AnalyzeGradArtMapping.LeftoverGradReqs("LowerMATH")
+# AnalyzeGradArtMapping.LeftoverGradReqs("LowerMathCS")  
+AnalyzeGradArtMapping.RankAgreementsperuni("LowerMathCS")
 # AnalyzeGradArtMapping.LeftoverGradReqs("UPPER") 
 # AnalyzeGradArtMapping.LeftoverGradReqs("UpperCS") 
 # AnalyzeGradArtMapping.LeftoverGradReqs("UpperMATH") 
