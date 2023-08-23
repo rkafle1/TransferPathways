5f02db4a3eeaba1743ad412b64c2f4feff90c481
Simone <gaminggyrrl@gmail.com>
2023-08-23 13:59:06 -0700

    removed dead code and added more comments

--- graduationReqs/split_course_types.py
+++ graduationReqs/split_course_types.py
@@ -23,6 +23,7 @@ def count_courses(line):
 
     return min_course_count
 
+# Counts the minimum number of courses that can be taken and puts the result in a dictionary
 def get_course_count(csv_path, exclude, count_dict):
     with open(csv_path, 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
@@ -57,6 +58,7 @@ def get_course_count(csv_path, exclude, count_dict):
         # get last schools unit count
         count_dict[prev_school] = count
 
+# Returns the minimum number units that could be comleted
 def count_units(line):
     units = "\d+\.\d+" # units will be floating point numbers
 
@@ -82,7 +84,7 @@ def count_units(line):
 
     return min_units_count
 
-
+# Calculates minimum number of units required for each school and writes the results to a CSV file
 def write_all(csv_path, name, count_dict):
     with open(csv_path, 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
@@ -120,7 +122,7 @@ def write_all(csv_path, name, count_dict):
             csv_writer.writerow( [prev_school, "Total units: " + str(unit_count)] )
             count_dict[prev_school] = unit_count
 
-
+# Calculates minimum number of units required for a certain subject (i.e. math, science, etc.) for each school and puts result in a dictionary
 def get_type_unit_count(csv_path, matches, exclude, count_dict):
     with open(csv_path, 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
@@ -156,7 +158,8 @@ def get_type_unit_count(csv_path, matches, exclude, count_dict):
         # get last schools unit count
         count_dict[prev_school] = unit_count
 
-
+# Calculates minimum number of units required for a certain subject (i.e. math, science, etc.) for each school
+# writes the results to a CSV file and in a dictionary
 def write_type_unit_count(csv_path, name, matches, exclude, count_dict):
     with open(csv_path, 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
@@ -211,3 +214,82 @@ if __name__ == '__main__':
     cs_matches   = ['CS ', 'CSE ', 'ECS ', 'EEC ', 'I&C ', 'IN4MATX ', 'COM SCI ', 'CMPSC ', 'CPSC ', 'COMP ', 'CSCI', 'CSCI/EECE ', 'CINS ', 'CSC ', 'CPE ', 'CECS ', 'CMPS ', 'CST ', 'ENG ', 'ENGR ', 'ECE ', 'EE ', 'Discrete ', 'Mathematical Structures I']
     math_matches = ['Math ', 'MATH ', 'MAT ', 'PSTAT ', 'STATS ', 'STAT ', 'STA ', 'E E ']
     sci_matches  = ['Physics ', 'physics', 'PHYSICS ', 'PHYS ', 'PHY ', 'BIO ', 'BIOL ', 'CHEM ', 'CHE ', 'Chem ', 'ELECTIVE SCIENCE:']
+
+    lower_cs_count = {}
+    lower_math_count = {}
+    lower_sci_count = {}
+    lower_other_count = {}
+    get_type_unit_count('CSVs/gradReqsLOWER.csv', cs_matches  , sci_matches + ['Linear Algebra', 'Statistics'], lower_cs_count)
+    get_type_unit_count('CSVs/gradReqsLOWER.csv', math_matches, sci_matches + ['Discrete', 'Mathematical Structures I'], lower_math_count)
+    get_type_unit_count('CSVs/gradReqsLOWER.csv', sci_matches , [], lower_sci_count)
+    get_type_unit_count('CSVs/gradReqsLOWER.csv', ['PHIL ', 'ENGL '], cs_matches + math_matches + sci_matches, lower_other_count)
+
+    upper_cs_count = {}
+    cs_elective_count = {}
+    upper_math_count = {}
+    upper_sci_count = {}
+    upper_other_count = {}
+    get_type_unit_count('CSVs/gradReqsUPPER.csv', cs_matches  , ['ELECTIVE:', 'Statistics', 'Probability'], upper_cs_count)
+    get_type_unit_count('CSVs/gradReqsUPPER.csv', ['ELECTIVE: '], [], cs_elective_count)
+    get_type_unit_count('CSVs/gradReqsUPPER.csv', math_matches, ['Discrete'], upper_math_count)
+    get_type_unit_count('CSVs/gradReqsUPPER.csv', sci_matches , [], upper_sci_count)
+    get_type_unit_count('CSVs/gradReqsUPPER.csv', ['PHIL ', 'ENGL ', 'ES/WGQS '], cs_matches + math_matches + sci_matches, upper_other_count)
+
+    # Convert semester units to quarter units
+    for school in lower_cs_count:
+        if "CSU" in school or "CP" in school or "State" in school or "Berkeley" in school or "Merced" in school:
+            if "San Luis Obispo" not in school:
+                lower_cs_count[school]    *= 1.5
+                lower_math_count[school]  *= 1.5
+                lower_sci_count[school]   *= 1.5
+                lower_other_count[school] *= 1.5
+                upper_cs_count[school]    *= 1.5
+                cs_elective_count[school] *= 1.5
+                upper_math_count[school]  *= 1.5
+                upper_sci_count[school]   *= 1.5
+                upper_other_count[school] *= 1.5
+
+    # Create a stacked bar chart with the data in the dictionaries
+    
+    lower_cs = list(lower_cs_count.values())
+    upper_cs = list(upper_cs_count.values())
+    elective_cs = list(cs_elective_count.values())
+
+    lower_other = list(lower_other_count.values())
+    upper_other = list(upper_other_count.values())
+    other = list(np.add(lower_other, upper_other))
+
+    lower_math = list(lower_math_count.values())
+    upper_math = list(upper_math_count.values())
+    math = list(np.add(lower_math, upper_math))
+
+    lower_sci = list(lower_sci_count.values())
+    upper_sci = list(upper_sci_count.values())
+    science = list(np.add(lower_sci, upper_sci))
+
+    labels = list(lower_cs_count.keys())
+
+    font = {'size' : 16}
+    plt.rc('font', **font)
+
+    plt.bar(labels, lower_cs, label="CS Lower Division")
+    plt.bar(labels, upper_cs, bottom=lower_cs, label="CS Required Upper Division")
+
+    bot = list(np.add(lower_cs, upper_cs))
+    plt.bar(labels, elective_cs, bottom=bot, label="CS Elective Upper Division")
+
+    bot = list(np.add(bot, elective_cs))
+    plt.bar(labels, math, bottom=bot, label="Math")
+
+    bot = list(np.add(bot, math))
+    plt.bar(labels, science, bottom=bot, label="Science")
+
+    bot = list(np.add(bot, science))
+    plt.bar(labels, other, bottom=bot, label="Other")
+
+    plt.ylabel('Quarter Units')
+    plt.xticks(rotation=45, ha='right')
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.1), ncol=2, fancybox=True, shadow=True)
+
+    plt.show()
+
